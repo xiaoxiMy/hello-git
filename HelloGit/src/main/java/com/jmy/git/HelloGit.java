0f209a2c5fcb0bd57b445bd64653bec62b402c27
Jiang Meng Ying <jiangmy26648@yunrong.cn>
2024-03-12 16:44:38 +0800

    创建分支

--- HelloGit/src/main/java/com/jmy/git/HelloGit.java
+++ HelloGit/src/main/java/com/jmy/git/HelloGit.java
@@ -14,5 +14,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("update one");
+        System.out.println("创建分支！");
     }
 }

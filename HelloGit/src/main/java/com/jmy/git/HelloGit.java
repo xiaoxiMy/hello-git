16fc83c0f2fe8dd096a43b295ca947cd110bfb34
Jiang Meng Ying <jiangmy26648@yunrong.cn>
2024-03-12 16:50:41 +0800

    主干添加

--- HelloGit/src/main/java/com/jmy/git/HelloGit.java
+++ HelloGit/src/main/java/com/jmy/git/HelloGit.java
@@ -15,5 +15,6 @@ public class HelloGit {
         System.out.println("hello git!");
         System.out.println("update one");
         System.out.println("创建分支！");
+        System.out.println("主干添加！");
     }
 }

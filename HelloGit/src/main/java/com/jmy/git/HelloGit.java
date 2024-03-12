f8290db3b41cbe17e1ce6b04425ebdab1e3e12b9
Jiang Meng Ying <jiangmy26648@yunrong.cn>
2024-03-12 16:26:26 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/jmy/git/HelloGit.java
@@ -0,0 +1,17 @@
+package com.jmy.git;
+
+/**
+ * PackageName com.jmy.git
+ * ClassName HelloGit
+ * Description HelloGit类用于...
+ *
+ * @version 1.0
+ * @Author MY.J
+ * @Email 3108079191@qq.com
+ * @Date 2024/3/12 16:14
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
